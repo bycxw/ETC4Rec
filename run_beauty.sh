f312fdb034bfc5e128273b2a41ea13567d5dee24
bycxw <beyondchenxi@163.com>
2021-03-21 20:46:04 +0800

    bert beauty 100000

--- run_beauty.sh
+++ run_beauty.sh
@@ -7,7 +7,7 @@ masked_lm_prob=0.6
 
 dim=64
 batch_size=256
-num_train_steps=10000
+num_train_steps=100000
 
 mask_prob=1.0
 prop_sliding_window=0.1
@@ -17,17 +17,17 @@ pool_size=10
 signature="-mp${mask_prob}-sw${prop_sliding_window}-mlp${masked_lm_prob}-df${dupe_factor}-mpps${max_predictions_per_seq}-msl${max_seq_length}"
 
 
-python -u gen_data_fin.py \
-    --dataset_name=${dataset_name} \
-    --data_dir=${DATA_DIR} \
-    --max_seq_length=${max_seq_length} \
-    --max_predictions_per_seq=${max_predictions_per_seq} \
-    --mask_prob=${mask_prob} \
-    --dupe_factor=${dupe_factor} \
-    --masked_lm_prob=${masked_lm_prob} \
-    --prop_sliding_window=${prop_sliding_window} \
-    --signature=${signature} \
-    --pool_size=${pool_size} \
+#python -u gen_data_fin.py \
+#    --dataset_name=${dataset_name} \
+#    --data_dir=${DATA_DIR} \
+#    --max_seq_length=${max_seq_length} \
+#    --max_predictions_per_seq=${max_predictions_per_seq} \
+#    --mask_prob=${mask_prob} \
+#    --dupe_factor=${dupe_factor} \
+#    --masked_lm_prob=${masked_lm_prob} \
+#    --prop_sliding_window=${prop_sliding_window} \
+#    --signature=${signature} \
+#    --pool_size=${pool_size} \
 
 
 CUDA_VISIBLE_DEVICES=1 python -u run.py \
