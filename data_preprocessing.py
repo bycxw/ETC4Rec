da078f448be1ef502285eabbace87a0f23c50d91
bycxw <beyondchenxi@163.com>
2021-03-28 11:55:50 +0800

    process raw ml data

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -11,7 +11,7 @@ from pathlib import Path
 from tqdm import tqdm
 
 class UserItemVocab(object):
-    def __inti__(self):
+    def __init__(self):
         self.user = dict()
         self.item = dict()
 
@@ -20,7 +20,7 @@ class UserItemVocab(object):
             self.user[user] = len(self.user) + 1
 
     def update_item(self, item):
-        if item in self.item:
+        if item not in self.item:
             self.item[item] = len(self.item) + 1
 
     def convert_user_to_id(self, user):
@@ -28,6 +28,13 @@ class UserItemVocab(object):
 
     def convert_item_to_id(self, item):
         return self.item.get(item, None)
+
+    def get_user_count(self):
+        return len(self.user)
+
+    def get_item_count(self):
+        return len(self.item)
+
 
 
 def process_raw_ml(in_file_path: Path, out_file_path: Path, min_rating_num: int = 0):
@@ -59,13 +66,14 @@ def process_raw_ml(in_file_path: Path, out_file_path: Path, min_rating_num: int 
                         out_f.write("{},{}\n".format(last_userid, one_rating.movieid))
                 last_userid = userid
                 cnt = 1
-        print("Done")
+        print("Process raw ml data done.")
         print("Processed file: {}".format(out_file_path))
 
 def build_vocab(file_path: Path):
     """
     Build vocab for interaction data.
     """
+    print("Start build vocab...")
     vocab = UserItemVocab()
     with open(file_path, "r", encoding="utf-8") as f:
         for line in f:
@@ -73,6 +81,8 @@ def build_vocab(file_path: Path):
             user, movie = line
             vocab.update_user(user)
             vocab.update_item(movie)
+    print("Build vocab done.")
+    print("user num: {}, item num: {}".format(vocab.get_user_count(), vocab.get_item_count()))
     return vocab
 
 
@@ -81,6 +91,7 @@ def build_data(in_file_path: Path, out_file_path: Path, vocab: UserItemVocab):
     build data for model.
     Convert user/item name to id.
     """
+    print("Start build data...")
     with open(in_file_path, "r", encoding="utf-8") as in_f, open(out_file_path, "w", encoding="utf-8") as out_f:
         for line in in_f:
             line = line.strip().split(",")
@@ -89,21 +100,23 @@ def build_data(in_file_path: Path, out_file_path: Path, vocab: UserItemVocab):
             if not user_id or not movie_id:
                 continue
             out_f.write("{},{}\n".format(user_id, movie_id))
+    print("Build data done.")
+    print("data file: {}".format(out_file_path))
 
 
-def ml_preprocess(in_file_path, out_file_path, min_rating_num=0):
-    tmp_file_path = out_file_path + ".tmp"
+def ml_preprocess(in_file_path: Path, out_file_path: Path, min_rating_num: int = 0):
+
+    tmp_file_path = Path(out_file_path.parent, out_file_path.name + ".tmp")
     process_raw_ml(in_file_path=in_file_path, out_file_path=tmp_file_path, min_rating_num=min_rating_num)
     vocab = build_vocab(file_path=tmp_file_path)
     build_data(in_file_path=tmp_file_path, out_file_path=out_file_path, vocab=vocab)
     os.remove(tmp_file_path)
 
 
-
 if __name__ == "__main__":
     MIN_RATING_NUM = 0
     in_file_path = Path("..", "datasets", "ml-25m", "ratings.csv")
-    out_file_path = Path("data", "ml-25m-{}.txt".format(MIN_RATING_NUM))
+    out_file_path = Path("..", "datasets", "ml-25m", "ml-25m-{}.txt".format(MIN_RATING_NUM))
     ml_preprocess(in_file_path=in_file_path,
                   out_file_path=out_file_path,
                   min_rating_num=MIN_RATING_NUM)
