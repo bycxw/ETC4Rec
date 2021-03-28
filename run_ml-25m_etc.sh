ade2d1ab2a73ef815457c7ba128874b18be79416
bycxw <beyondchenxi@163.com>
2021-03-28 12:07:44 +0800

    add ml-25m config

--- run_ml-25m_etc.sh
+++ run_ml-25m_etc.sh
@@ -3,8 +3,8 @@ DATA_DIR="/home/mist/data"
 dataset_name="ml-25m-0"
 max_seq_length=200
 masked_lm_prob=0.2
-global_seq_length=1
-local_radius=1
+global_seq_length=16
+local_radius=16
 max_predictions_per_seq=40
 
 dim=64
@@ -31,27 +31,27 @@ python -u gen_data_fin.py \
     --signature=${signature} \
     --pool_size=${pool_size} \
 
-CUDA_VISIBLE_DEVICES=0 python -u run.py \
-    --train_input_file=${DATA_DIR}/${dataset_name}${signature}.train.tfrecord \
-    --test_input_file=${DATA_DIR}/${dataset_name}${signature}.test.tfrecord \
-    --vocab_filename=${DATA_DIR}/${dataset_name}${signature}.vocab \
-    --user_history_filename=${DATA_DIR}/${dataset_name}${signature}.his \
-    --checkpointDir=${CKPT_DIR}/${dataset_name} \
-    --signature=${signature}-${dim} \
-    --do_train=True \
-    --do_eval=True \
-    --model_config_file=./train_config/etc_config_${dataset_name}_${dim}.json \
-    --batch_size=${batch_size} \
-    --max_seq_length=${max_seq_length} \
-    --global_seq_length=${global_seq_length} \
-    --max_predictions_per_seq=${max_predictions_per_seq} \
-    --num_train_steps=${num_train_steps} \
-    --num_warmup_steps=100 \
-    --learning_rate=1e-4
+#CUDA_VISIBLE_DEVICES=0 python -u run.py \
+#    --train_input_file=${DATA_DIR}/${dataset_name}${signature}.train.tfrecord \
+#    --test_input_file=${DATA_DIR}/${dataset_name}${signature}.test.tfrecord \
+#    --vocab_filename=${DATA_DIR}/${dataset_name}${signature}.vocab \
+#    --user_history_filename=${DATA_DIR}/${dataset_name}${signature}.his \
+#    --checkpointDir=${CKPT_DIR}/${dataset_name} \
+#    --signature=${signature}-${dim} \
+#    --do_train=True \
+#    --do_eval=True \
+#    --model_config_file=./train_config/etc_config_${dataset_name}_${dim}.json \
+#    --batch_size=${batch_size} \
+#    --max_seq_length=${max_seq_length} \
+#    --global_seq_length=${global_seq_length} \
+#    --max_predictions_per_seq=${max_predictions_per_seq} \
+#    --num_train_steps=${num_train_steps} \
+#    --num_warmup_steps=100 \
+#    --learning_rate=1e-4
 
 
-mkdir /home/mist/cloud/${dataset_name}${signature}-${dim}
-cp ${CKPT_DIR}/${dataset_name}${signature}-${dim}/eval_results.txt /home/mist/cloud/${dataset_name}${signature}-${dim}
+#mkdir /home/mist/cloud/${dataset_name}${signature}-${dim}
+#cp ${CKPT_DIR}/${dataset_name}${signature}-${dim}/eval_results.txt /home/mist/cloud/${dataset_name}${signature}-${dim}
 
 
 #sleep 300
