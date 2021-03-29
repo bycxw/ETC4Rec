017de85c60898c1abe22dea4ae225e1ea78205c2
bycxw <beyondchenxi@163.com>
2021-03-29 22:06:35 +0800

    config for ml-25m-100/500

--- run_ml-25m-500.sh
+++ run_ml-25m-500.sh
@@ -1,5 +1,6 @@
 CKPT_DIR="/home/mist/data/record"
 DATA_DIR="/home/mist/data"
+#DATA_DIR="/mnt/c/Data/dissertation/datasets/ml-25m"
 dataset_name="ml-25m-500"
 max_seq_length=200
 masked_lm_prob=0.2
@@ -30,28 +31,28 @@ python -u gen_data_fin.py \
     --pool_size=${pool_size} \
 
 
-#CUDA_VISIBLE_DEVICES=0 python -u run.py \
-#    --train_input_file=${DATA_DIR}/${dataset_name}${signature}.train.tfrecord \
-#    --test_input_file=${DATA_DIR}/${dataset_name}${signature}.valid.tfrecord \
-#    --vocab_filename=${DATA_DIR}/${dataset_name}${signature}.vocab \
-#    --user_history_filename=${DATA_DIR}/${dataset_name}${signature}.valid.his \
-#    --checkpointDir=${CKPT_DIR}/${dataset_name} \
-#    --signature=${signature}-${dim} \
-#    --do_train=True \
-#    --do_eval=True \
-#    --model_config_file=./train_config/bert_config_${dataset_name}_${dim}.json \
-#    --batch_size=${batch_size} \
-#    --max_seq_length=${max_seq_length} \
-#    --max_predictions_per_seq=${max_predictions_per_seq} \
-#    --num_train_steps=${num_train_steps} \
-#    --num_warmup_steps=100 \
-#    --learning_rate=1e-4
-#
-#
-#mkdir /home/mist/cloud/${dataset_name}${signature}-${dim}
-#cp ${CKPT_DIR}/${dataset_name}${signature}-${dim}/eval_results.txt /home/mist/cloud/${dataset_name}${signature}-${dim}
-#cp nohup.out /home/mist/cloud/${dataset_name}${signature}-${dim}
-#remove nohup.out
-#
-#sleep 300
-#sh ../shutdown.sh
+CUDA_VISIBLE_DEVICES=0 python -u run.py \
+    --train_input_file=${DATA_DIR}/${dataset_name}${signature}.train.tfrecord \
+    --test_input_file=${DATA_DIR}/${dataset_name}${signature}.valid.tfrecord \
+    --vocab_filename=${DATA_DIR}/${dataset_name}${signature}.vocab \
+    --user_history_filename=${DATA_DIR}/${dataset_name}${signature}.valid.his \
+    --checkpointDir=${CKPT_DIR}/${dataset_name} \
+    --signature=${signature}-${dim} \
+    --do_train=True \
+    --do_eval=True \
+    --model_config_file=./train_config/bert_config_${dataset_name}_${dim}.json \
+    --batch_size=${batch_size} \
+    --max_seq_length=${max_seq_length} \
+    --max_predictions_per_seq=${max_predictions_per_seq} \
+    --num_train_steps=${num_train_steps} \
+    --num_warmup_steps=100 \
+    --learning_rate=1e-4
+
+
+mkdir /home/mist/cloud/${dataset_name}${signature}-${dim}
+cp ${CKPT_DIR}/${dataset_name}${signature}-${dim}/eval_results.txt /home/mist/cloud/${dataset_name}${signature}-${dim}
+cp nohup.out /home/mist/cloud/${dataset_name}${signature}-${dim}
+remove nohup.out
+
+sleep 300
+sh ../shutdown.sh
