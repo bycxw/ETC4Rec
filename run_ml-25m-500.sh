3692818ce2cc373751a49fbd7da5d77205e3521d
bycxw <beyondchenxi@163.com>
2021-03-31 16:41:08 +0800

    exp for bert ml-25m-500 msl=1024

--- run_ml-25m-500.sh
+++ run_ml-25m-500.sh
@@ -2,9 +2,9 @@ CKPT_DIR="/home/mist/data/record"
 DATA_DIR="/home/mist/data"
 #DATA_DIR="/mnt/c/Data/dissertation/datasets/ml-25m"
 dataset_name="ml-25m-500"
-max_seq_length=200
+max_seq_length=1024
 masked_lm_prob=0.2
-max_predictions_per_seq=40
+max_predictions_per_seq=205
 
 dim=64
 batch_size=64
